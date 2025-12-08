6962199096f64bdf9554a16d590d6669f5f359d0
Rafael Schecheleck Muczinski <muczinskirafael@gmail.com>
2025-12-08 12:56:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efeito de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
