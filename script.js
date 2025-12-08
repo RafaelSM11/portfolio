e2147e3d59f65e0f07a5fda35b101c08cc3d990c
Rafael Schecheleck Muczinski <muczinskirafael@gmail.com>
2025-12-08 14:04:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
         });
     });
 });
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": { "enable": true, "value_area": 800 }
+    },
+    "color": { "value": "#08f7fe" },
+    "shape": { "type": "circle" },
+    "opacity": {
+      "value": 0.5,
+      "random": true
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#08f7fe",
+      "opacity": 0.3,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "grab" }
+    }
+  },
+  "retina_detect": true
+});
