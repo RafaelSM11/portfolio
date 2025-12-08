c06ecbb96619cc7d413adfa906c501960844d998
Rafael Schecheleck Muczinski <muczinskirafael@gmail.com>
2025-12-08 14:16:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -43,3 +43,11 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('open');
+});
